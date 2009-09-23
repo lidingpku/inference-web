78de0d21da24548f031906df2499673ac198cafc
huangr3 <huangr3@b7b0b90b-7027-0410-bdf8-abaac9723eb6>
2009-09-23 21:09:29 +0000

    change formula_info mapping from HashMap to DataPVHMap

--- trunk/iw3/src/org/inference_web/iwapp/hypergraph/DataPmlInfoMapping.java
+++ trunk/iw3/src/org/inference_web/iwapp/hypergraph/DataPmlInfoMapping.java
@@ -1,6 +1,5 @@
 package org.inference_web.iwapp.hypergraph;
 
-import java.util.HashMap;
 import java.util.Set;
 
 import com.hp.hpl.jena.rdf.model.Model;
@@ -13,11 +12,11 @@ import com.hp.hpl.jena.vocabulary.OWL;
 import sw4j.app.pml.PMLJ;
 import sw4j.app.pml.PMLP;
 import sw4j.rdf.util.ToolJena;
+import sw4j.util.DataPVHMap;
 import sw4j.util.DataPVTMap;
-import sw4j.util.ToolString;
 
 public class DataPmlInfoMapping {
-	HashMap<String, Resource> map_formula_info = new HashMap<String, Resource>();
+	DataPVHMap<String, Resource> map_formula_info = new DataPVHMap<String, Resource>();
 
 	public void add_model(Model m){
 		if (null==m)
@@ -29,7 +28,7 @@ public class DataPmlInfoMapping {
 			if (!nodes.contains(stmt.getSubject()))
 				continue;
 			
-			map_formula_info.put(ToolJena.getNodeString(stmt.getObject()), stmt.getSubject());
+			map_formula_info.add(ToolJena.getNodeString(stmt.getObject()), stmt.getSubject());
 		}
 	}
 	
@@ -41,16 +40,16 @@ public class DataPmlInfoMapping {
 			Set<String> set_formula = map_norm_formula.getValuesAsSet(norm);
 			Resource res_info_root = null;
 			for (String formula: set_formula){
-				Resource res_info = map_formula_info.get(formula);
-				
-				if (null==res_info_root)
-					res_info_root = res_info;
-				else
-					model_mappings.add(model_mappings.createStatement(res_info_root, OWL.sameAs, res_info));
+				for (Resource res_info: map_formula_info.getValuesAsSet(formula)){
+					if (null==res_info_root)
+						res_info_root = res_info;
+					else
+						model_mappings.add(model_mappings.createStatement(res_info_root, OWL.sameAs, res_info));
+				}
 			}
 		}
 
-		System.out.println(ToolString.printCollectionToString(map_norm_formula.keySet()));
+//		System.out.println(ToolString.printCollectionToString(map_norm_formula.keySet()));
 
 		return model_mappings;
 
