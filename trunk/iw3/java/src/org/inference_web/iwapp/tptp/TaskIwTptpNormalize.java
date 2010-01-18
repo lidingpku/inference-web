a5a714c53bffa4be149068584eba7c4827ac55ef
lidingpku <lidingpku@b7b0b90b-7027-0410-bdf8-abaac9723eb6>
2010-01-18 09:13:50 +0000

    skip large pml files

--- trunk/iw3/java/src/org/inference_web/iwapp/tptp/TaskIwTptpNormalize.java
+++ trunk/iw3/java/src/org/inference_web/iwapp/tptp/TaskIwTptpNormalize.java
@@ -4,9 +4,12 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
 import org.inference_web.pml.ToolPml;
 
 import sw4j.app.pml.PMLJ;
+import sw4j.util.Sw4jException;
+import sw4j.util.ToolIO;
 import sw4j.util.ToolString;
 
 import com.hp.hpl.jena.rdf.model.Model;
@@ -47,13 +50,27 @@ public class TaskIwTptpNormalize extends AgentIwTptp{
 	
 	void run_normalize(){
 		for (String sz_url_pml: this.set_url_pml){
-			System.out.println("processing: "+sz_url_pml);
+			Logger.getLogger(this.getClass()).info("processing: "+sz_url_pml);
 
 			Model m = ModelFactory.createDefaultModel();
 			m.read(sz_url_pml);
 			
 			if (!m.listSubjectsWithProperty(RDF.type, PMLJ.InferenceStep).hasNext()){
 				System.out.println("skipped (no pml inference step): "+sz_url_pml);
+				continue;
+			}
+			
+			Logger.getLogger(this.getClass()).info(" number of triples: "+ m.size());
+			
+			//skip very large files
+			if (m.size()>100000){
+				Logger.getLogger(this.getClass()).warn(" large file, skipped: "+ sz_url_pml);
+				File f_output_pml = new File(dir_root_output, "skipped-pml.csv");
+				try {
+					ToolIO.pipeStringToFile(sz_url_pml+","+ m.size(), f_output_pml, false, true);
+				} catch (Sw4jException e) {
+					e.printStackTrace();
+				}
 				continue;
 			}
 
